207ff6114822a69b8f23972a999110731b4664e2
Dibyo178 <Sourov.mu@gmail.com>
2021-06-09 19:20:30 +0600

    doen

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ const displayCountries =(countries)=>{
     // }
 }
 
+//  display all country names...
+
 const displayCountryDetail = (name)=>{
   const url=`https://restcountries.eu/rest/v2/name/${name}`
   fetch(url)
